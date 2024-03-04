3713a07baa83984e2034f67ea8c9a125f9efc8ce
amir fattahi <136432203+amirdity@users.noreply.github.com>
2024-03-04 13:02:30 +0330

    Add files via upload

---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+function Header (){
+  return(
+    <header>
+      <h1>my web site </h1>
+      <nav>
+        <ol>
+          <li><a hreh="#"> home</a></li>
+          <li><a hreh="#"> home</a></li>
+          <li><a hreh="#"> home</a></li>
+          <li><a hreh="#"> home</a></li>
+          <li><a hreh="#"> home</a></li>
+          <li><a hreh="#"> home</a></li>
+          <li><a hreh="#"> home</a></li>
+          <li><a hreh="#"> home</a></li>
+          <li><a hreh="#"> home</a></li>
+          <li><a hreh="#"> home</a></li>
+        </ol>
+      </nav>
+      <hr/>
+    </header>
+  );
+}
+export default Header
