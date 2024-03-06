a3ce6239b5f5c7e0c6828ade915f7dd86cd847fa
amir fattahi <amirfattahi22@gmail.com>
2024-03-06 11:39:47 +0330

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import Button from './button';
+
 function App() {
+  class Car {
+    constructor (name) {
+      this.brand = name;
+    }
+  }
+  const myCar = new Car("ford")
+  const myArray = ["apple","banana","watermelon"]
+  const List = myArray.map((item)=> <p> ${item}</p>)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p tabIndex={1}>
+          FREE CODE CAMPE 
         </p>
-        <Button/>
+        <p tabIndex={5}>
+          FREE CODE CAMPE 
+        </p>
+        <p tabIndex={3}>
+          FREE CODE CAMPE 
+        </p>
+        <p tabIndex={2}>
+          FREE CODE CAMPE 
+        </p>
+        <List/>
         <a
           className="App-link"
           href="https://reactjs.org"
