4d0704668d7479e82405983f26c9971bbca6b29a
amir fattahi <amirfattahi22@gmail.com>
2024-03-11 13:39:50 +0330

    accordion
    finally my first project with tailwind
    it take me so much time to learn

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -9,7 +9,13 @@ export default function Accordian() {
   function handleSingleSelection(getCurrentId) {
     setSelected(getCurrentId === selected ? null : getCurrentId);
   }
-  function handleMultiSelection(getCurrentId) {}
+  function handleMultiSelection(getCurrentId) {
+    let copyMultiple = [...multiple];
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+    if (findIndexOfCurrentId === -1) copyMultiple.push(getCurrentId);
+    else copyMultiple.splice(findIndexOfCurrentId, 1);
+    setMultiple(copyMultiple);
+  }
   return (
     <>
       <div className="flex justify-center align-justify bg-purple-600 font-roboto text-sm">
@@ -19,16 +25,26 @@ export default function Accordian() {
               <div className="">
                 <div
                   className="flex align-middle m-5 justify-between  bg-cyan-500 rounded-lg p-1 cursor-zoom-in"
-                  onClick={() => handleSingleSelection(dataItem.id)}
+                  onClick={
+                    enableMultiSelection
+                      ? () => handleMultiSelection(dataItem.id)
+                      : () => handleSingleSelection(dataItem.id)
+                  }
                 >
                   <h3 className="">{dataItem.question}</h3>
                   <span> + </span>
                 </div>
-                {selected === dataItem.id ? (
-                  <div className="flex  align-middle m-5 justify-between p-1 max-w-[90%] pl-5 ">
-                    <p>{dataItem.answer}</p>
-                  </div>
-                ) : null}
+                {enableMultiSelection
+                  ? multiple.indexOf(dataItem.id) !== -1 && (
+                      <div className="flex align-middle m-5 justify-between p-1 max-w-[90%] pl-5">
+                        {dataItem.answer}
+                      </div>
+                    )
+                  : selected === dataItem.id && (
+                      <div className="flex align-middle m-5 justify-between p-1 max-w-[90%] pl-5">
+                        {dataItem.answer}
+                      </div>
+                    )}
               </div>
             ))
           ) : (
@@ -37,8 +53,11 @@ export default function Accordian() {
         </div>
       </div>
       <div className="font-roboto text-sm flex  justify-center">
-        <button className="bg-cyan-500 rounded-2xl p-1 px-2 mt-1 shadow-2xl shadow-blue-600/100   transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
-          enable it and check the answer
+        <button
+          onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+          className="bg-cyan-500 rounded-2xl p-1 px-3 mt-1 shadow-2xl shadow-blue-600/100   transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+        >
+          enable it and check the answers
         </button>
       </div>
     </>
