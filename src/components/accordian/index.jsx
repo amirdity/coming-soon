a760760d3f25737cd09351e28e6d1128e265ffcf
amir fattahi <amirfattahi22@gmail.com>
2024-03-11 12:50:35 +0330

    Add files via upload

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -3,41 +3,44 @@ import data from "./data";
 
 export default function Accordian() {
   const [selected, setSelected] = useState(null);
-  function handleSingleSelection(getCurrentId) {
-    setSelected(getCurrentId);
-  }
-  return (
-    <div className="">
-      <div className="">
-        {data && data.length > 0 ? (
-          <div>
-            {data.map((getItem) => (
-              <div>
-                <h3>{getItem.question} </h3> <span>+</span>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p>no data found</p>
-        )}
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
-        {/*  {data && data.length > 0 ? (
-          data.map((dataItem) => (
-            <div className="">
-              <div
-                className=""
-                onClick={() => handleSingleSelection(dataItem.id)}
-              >
-                <h3>{dataItem.question}</h3>
-                <span>+</span>
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+  function handleMultiSelection(getCurrentId) {}
+  return (
+    <>
+      <div className="flex justify-center align-justify bg-purple-600 font-roboto text-sm">
+        <div className=" basis-5/6">
+          {data && data.length > 0 ? (
+            data.map((dataItem) => (
+              <div className="">
+                <div
+                  className="flex align-middle m-5 justify-between  bg-cyan-500 rounded-lg p-1 cursor-zoom-in"
+                  onClick={() => handleSingleSelection(dataItem.id)}
+                >
+                  <h3 className="">{dataItem.question}</h3>
+                  <span> + </span>
+                </div>
+                {selected === dataItem.id ? (
+                  <div className="flex  align-middle m-5 justify-between p-1 max-w-[90%] pl-5 ">
+                    <p>{dataItem.answer}</p>
+                  </div>
+                ) : null}
               </div>
-              {selected === dataItem.id ? <div>{dataItem.answer}</div> : null}
-            </div>
-          ))
-        ) : (
-          <div>no data found!</div>
-        )}   */}
+            ))
+          ) : (
+            <div>no data found!</div>
+          )}
+        </div>
       </div>
-    </div>
+      <div className="font-roboto text-sm flex  justify-center">
+        <button className="bg-cyan-500 rounded-2xl p-1 px-2 mt-1 shadow-2xl shadow-blue-600/100   transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+          enable it and check the answer
+        </button>
+      </div>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontSize: {
+      'sm': '2vw',
+     'base': '1rem',
+      'xl': '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    fontFamily: {
+      'sans': ['Helvetica', 'Arial', 'sans-serif'],
+      'roboto':["Roboto", 'sans-serif'],
+    },
     screens: {
       'sm': { 'max': '767px'},
       // => @media (min-width: 640px and max-width: 767px) { ... }
@@ -22,7 +35,15 @@ module.exports = {
       '5': '32px',
       '6': '48px',
     },
-    extend: {       
+    extend: {    
+      flexBasis: {
+        '1/7': '14.2857143%',
+        '2/7': '28.5714286%',
+        '3/7': '42.8571429%',
+        '4/7': '57.1428571%',
+        '5/7': '71.4285714%',
+        '6/7': '85.7142857%',
+      },
       
     },
   },
