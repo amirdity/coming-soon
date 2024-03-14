d122b7ce046bc25f791e469154c72dec0d550be9
amir fattahi <amirfattahi22@gmail.com>
2024-03-14 05:27:02 +0330

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
-      <Accordian/>   
-      <RandomColor/>
+   {/*   <Accordian/>   
+      <RandomColor/>  */}
+      <StarRating/>
     </div>
   );
 }
---
+++ src/components/star-rating/index.jsx
@@ -0,0 +1,3 @@
+export default function StarRating() {
+  return <div></div>;
+}
